a90aa5f49ae6b0336b39415fb4029483f0759832
Nadavkeysar <74132825+Nadavkeysar@users.noreply.github.com>
2021-01-21 17:21:00 +0200

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,12 @@
+package maraton2;
+
+    public class Node {
+        int x, y, price, numOfPath;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+
